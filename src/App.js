4c4785d280fb57d038177a48ee8f2f3ef27c12d3
ChayaGreisman <chayamgreisman@gmail.com>
2020-09-07 20:49:35 -0400

    playing with ice cream modal

--- src/App.js
+++ src/App.js
@@ -10,6 +10,63 @@ function App() {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+  
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+  
+    if (this.isDeleting) { delta /= 2; }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+  
+  window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+  };
+
   return (
     <div className="App">
 
@@ -17,6 +74,17 @@ function App() {
       <NavBar handleShow={handleShow}/>
       
       <p className="typing">Hey! I'm Chaya.</p>
+
+
+      <h1>       
+        <span
+          className="txt-rotate"
+          data-period="2000"
+          data-rotate='[ "software engineer", "knowledge junkie", "self taught gymnast", "ice cream enthusiast", "creator" ]'>
+        </span> 
+      </h1>     
+
+
       <div id='about'>
           <img src="./mepic2.png" alt="chaya-greisman"/>
           <h1 style={{fontFamily: 'Raleway', fontSize: '10vw'}}>Hey! I'm Chaya.</h1>
--- src/components/IceCreamModal.js
+++ src/components/IceCreamModal.js
@@ -5,23 +5,23 @@ import {Modal, Button} from 'react-bootstrap';
 function IceCreamModal(props){
   return (
       <Modal show={props.show} onHide={props.handleClose} animation={false} centered>
-              <Modal.Header closeButton>
-                <Modal.Title>Modal heading</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>Woohoo, you're reading this text in a modal!fdbfdsdgnfggbdfvdsv
-                vfbdfnbggn g
-                <h1>sdfgawyivuyfcgei</h1>
-                <h1>sdfgawyivuyfcgei</h1>
-                <h1>sdfgawyivuyfcgei</h1>
-              </Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={props.handleClose}>
+              <Modal.Body>
+                  <strong>Favorite's gotta be mint chip ... yours?</strong>
+               <br/>
+               <br/>
+               <small><strong>Let me know:</strong></small>
+               <br/>
+               <a href = "mailto: chayamgreisman@gmail.com">
+                    <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-envelope" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/>
+                    </svg>
+                </a>
+               <br/>
+               <br/>
+                <Button className="ice-cream-close" onClick={props.handleClose}>
                   Close
                 </Button>
-                <Button variant="primary" onClick={props.handleClose}>
-                  Save Changes
-                </Button>
-              </Modal.Footer>
+              </Modal.Body>
       </Modal>
   );
 }
