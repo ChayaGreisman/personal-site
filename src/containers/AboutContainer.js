32e6d7043c5a936c472126d6d27b23cc3aaa8dba
ChayaGreisman <chayamgreisman@gmail.com>
2020-09-26 20:26:28 -0400

    about text

--- src/containers/AboutContainer.js
+++ src/containers/AboutContainer.js
@@ -5,9 +5,11 @@ function AboutContainer(){
     <div style={{paddingTop: "75px"}}>
         <img src="./mepic2.png" alt="chaya-greisman" className="chaya-greisman-pic"/>
         <div className = 'about-text'>
-          <h4>I'm Chaya, a Full Stack Developer with experience in Rails, JavaScript, React, and multiple CSS frameworks.</h4>
+          <h4>I'm Chaya, a Full Stack Developer with experience in Ruby, Rails, JavaScript, React, and multiple CSS frameworks.</h4>
           <br/>
           <h4>I'm passionate about clean design and delivering a full product that is both practical and intuitive.</h4>
+          <br/>
+          <h4>If I'm not coding, I'm probably reading the latest tech mogul biography or baking pumpkin muffins.</h4>
         </div>
         <br/>
     </div>
