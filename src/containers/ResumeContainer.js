e68f26544b690806aa67b67e1dca2f3bee73335a
ChayaGreisman <chayamgreisman@gmail.com>
2020-12-15 12:06:31 -0500

    change resume

--- src/containers/ResumeContainer.js
+++ src/containers/ResumeContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ResumeContainer(){
   return (
-    <iframe src="./resume.pdf" width="100%" height={window.screen.height - window.screen.height/5.5}></iframe>
+    <iframe src="./Chaya_Greisman_Resume.pdf" width="100%" height={window.screen.height - window.screen.height/5.5}></iframe>
   );
 }
 
