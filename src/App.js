aa42da8885c917e982acff3c4cd38d2b5d65997f
ChayaGreisman <chayamgreisman@gmail.com>
2021-03-25 18:36:36 -0700

    added sass, playing with cursor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import './App.scss';
 import NavBar from './components/Nav';
 import IceCreamModal from './components/IceCreamModal';
 import HomeContainer from './containers/HomeContainer';
